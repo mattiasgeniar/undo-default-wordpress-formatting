642fc0fbaff942aee2185857e5a2f945baa6b1ee
Mattias Geniar <m@ttias.be>
2013-05-07 09:11:36 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,52 @@
+Plugin Name
+===========
+
+Contributors: Mattiasgeniar
+
+Tags: dash, double dash, single dash, replace, htmlcharacter, quotes, curly quote, single quote, double quote, ending quote, opening quote
+
+Requires at least: 3.3
+
+Tested up to: 3.3.1
+
+Stable tag: 1.0
+
+This plugin undoes some of the default formatting that gets applied to new posts created via Wordpress.
+
+Description
+===========
+
+Wordpress applies default text formatting that replaces characters such as '-', '--', quotes, ... to pretty HTML characters. This plugin was created to revert that back. This is especially useful for tech-oriented blogs that need the original input because they may contain commands that need to be executed.
+
+Installation
+============
+
+1. upload the directory "undo_wordpress_default_formatting" to your wp-content/plugins directory
+
+2. activate the plugin from your Wordpress 'Plugins' menu
+
+Frequently Asked Questions
+==========================
+
+The characters get replaced to something else than my original, why is that?
+----------------------------------------------------------------------------
+
+Wordpress changes a few characters to the same HTML entity character. I can not guess what the original character was, so I'm using a 'best effort' way of converting the HTML character back to the original one. Sometimes, it'll be wrong.
+
+Changelog
+=========
+
+1.0
+---
+
+* First version of the Plugin, stable
+
+Upgrade Notice
+==============
+
+1.0
+---
+
+There is only 1 version so far, no upgrade notices yet.
+
+
---
+++ undo_wordpress_default_formatting.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Plugin Name: Undo Wordpress Default Formatting
+ * Description: Wordpress applies default text formatting that replaces characters such as '-', '--', quotes, ... to pretty HTML characters. This plugin was created to revert that back. This is especially useful for tech-oriented blogs that need the original input.
+ * Version: 1.0
+ * Author: Mattias Geniar
+ * Author URI: http://mattiasgeniar.be/
+ * */
+
+add_filter( 'the_content' , 'mg_undo_wordpress_default_formatting' , 99 );
+function mg_undo_wordpress_default_formatting ($text) {
+    /* Each htmlchar can only occur once here. That sucks, because wordpress converts different input
+     *      to the same HTML char. That means I have to decide what will be the most dominant output. */
+    $arrHtmlChars = array ( "&#8217;",    /* Single quote */
+        "&#8212;",  /* Em dash */
+        "&#8211;",  /* En dash */
+        "&#8230;",  /* A triple dot ... */
+        "&#8220;",  /* Opening curly quote */
+        "&#8221;",  /* Closing curly quote */
+      );
+        $arrSimpleChars = array("'",    /* Single quote */
+                  "---",    /* Em dash */
+                          "--",   /* En dash */
+                                  "...",    /* A triple dot ... */
+                                          "\"",   /* Opening curly quote */
+                                                  "\"",   /* Closing curly quote */
+                                                        );
+
+        /* Replace all occurences */
+        return str_replace( $arrHtmlChars, $arrSimpleChars, $text);
+        /* For debugging*/ 
+        /*return $text;*/
+}
+
